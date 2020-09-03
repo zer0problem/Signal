b9f3273ef4d05320486c9f06f2b92e2838d4ed20
zer0problem <allmynamearegone@gmail.com>
2020-09-03 10:07:53 +0200

    Updated Signal.h to use templates
    template parameter lets more be done in compile time than during runtime, using a function pointer instead of a virtual class removes the need for 1 vtable indirection and 1 vtable less is required to be stored in mySubscribers

--- Signal.h
+++ Signal.h
@@ -1,104 +1,107 @@
 // requires C++17
 // https://github.com/zer0problem/Signal/
 #pragma once
-#include <algorithm>
 #include <vector>
 #include <functional>
 
 template<class ...ArgumentTypes>
-class Signal {
+class Signal
+{
 public:
-	template<class SourceType>
-	void Connect(SourceType *aSource, void(SourceType:: *aCallback)(ArgumentTypes ...)) {
-		mySubscribers.push_back(DataObject());
-		new(&mySubscribers[mySubscribers.size() - 1]) MemberWrapper<SourceType>(aSource, aCallback);
-	};
-	template<class SourceType>
-	void Connect(SourceType *aSource, void(*aCallback)(SourceType *, ArgumentTypes ...)) {
-		mySubscribers.push_back(DataObject());
-		new(&mySubscribers[mySubscribers.size() - 1]) StaticWrapper<SourceType>(aSource, aCallback);
-	};
-	void Connect(void(*aCallback)(ArgumentTypes ...)) {
-		mySubscribers.push_back(DataObject());
-		new(&mySubscribers[mySubscribers.size() - 1]) GlobalWrapper(aCallback);
-	};
-	template<class SourceType>
-	void Disconnect(SourceType *aSource, void(SourceType:: *aCallback)(ArgumentTypes ...)) {
-		for(auto it = mySubscribers.begin(); it != mySubscribers.end(); it++) {
-			MemberWrapper<SourceType> *wrapper = reinterpret_cast<MemberWrapper<SourceType> *>(&(*it));
-			if(wrapper->mySource == aSource && wrapper->myCallback == aCallback) {
+	// matches the correct private Connect function, allows only sending the function in as a template argument instead of both Type, Function* and Source*
+	template<auto Callback, class SourceType>
+	inline void Connect(SourceType *aSource)
+	{
+		Connect<SourceType, Callback>(aSource);
+	}
+	// directly adds a sourceless or global function
+	template<void (*TemplateFunction)(ArgumentTypes...)>
+	void Connect()
+	{
+		mySubscribers.push_back(TemplateFunctionPair(reinterpret_cast<VoidFunctionPointer>(&GlobalFunction<TemplateFunction>), nullptr));
+	}
+	// matches the correct private Disconnect function, just like the Connect function
+	template<auto Callback, class SourceType>
+	inline void Disconnect(SourceType *aSource)
+	{
+		Disconnect<SourceType, Callback>(aSource);
+	}
+	// searches for and removes the first matching Connection of the type
+	template<void (*TemplateFunction)(ArgumentTypes...)>
+	void Disconnect()
+	{
+		for(auto it = mySubscribers.begin(); it != mySubscribers.end(); it++)
+		{
+			if(reinterpret_cast<VoidFunctionPointer>(&GlobalFunction<TemplateFunction>) == it->first)
+			{
 				mySubscribers.erase(it);
 				break;
 			}
 		}
-	};
-	template<class SourceType>
-	void Disconnect(SourceType *aSource, void(*aCallback)(SourceType *, ArgumentTypes ...)) {
-		for(auto it = mySubscribers.begin(); it != mySubscribers.end(); it++) {
-			StaticWrapper<SourceType> *wrapper = reinterpret_cast<StaticWrapper<SourceType> *>(&(*it));
-			if(wrapper->mySource == aSource && wrapper->myCallback == aCallback) {
-				mySubscribers.erase(it);
-				break;
-			}
-		}
-	};
-	void Disconnect(void(*aCallback)(ArgumentTypes ...)) {
-		for(auto it = mySubscribers.begin(); it != mySubscribers.end(); it++) {
-			GlobalWrapper *wrapper = reinterpret_cast<GlobalWrapper *>(&(*it));
-			if(wrapper->myCallback == aCallback) {
-				mySubscribers.erase(it);
-				break;
-			}
-		}
-	};
-	void Emit(ArgumentTypes ...aArguments) {
-		for(auto it = mySubscribers.begin(); it != mySubscribers.end(); it++) {
-			(*reinterpret_cast<BaseWrapper *>(&(*it)))(aArguments...);
+	}
+	void Emit(ArgumentTypes ...aArguments)
+	{
+		for(auto it = mySubscribers.begin(); it != mySubscribers.end(); it++)
+		{
+			(*it).first((*it).second, aArguments...);
 		}
 	}
 private:
-	class BaseWrapper {
-	public:
-		virtual void operator()(ArgumentTypes...) = 0;
-	};
-	template<class SourceType>
-	class MemberWrapper : public BaseWrapper {
-	public:
-		MemberWrapper(SourceType *aSource, void(SourceType:: *aCallback)(ArgumentTypes ...)) {
-			mySource = aSource;
-			myCallback = aCallback;
+	using VoidFunctionPointer = void(*)(void *, ArgumentTypes...);
+	using TemplateFunctionPair = std::pair<VoidFunctionPointer, void *>;
+	std::vector<TemplateFunctionPair> mySubscribers;
+
+	// these 3 template functions that the function pair is gonna point at, takes and resolves the correct function at compiling, avoiding vtables for virtual classes to target the correct function
+	template<class SourceType, void (SourceType:: *TemplateFunction)(ArgumentTypes...)>
+	static void MemberFunction(SourceType *aSource, ArgumentTypes... aArguments)
+	{
+		(aSource->*TemplateFunction)(aArguments...);
+	}
+	template<class SourceType, void (*TemplateFunction)(SourceType *, ArgumentTypes...)>
+	static void StaticFunction(SourceType *aSource, ArgumentTypes... aArguments)
+	{
+		(*TemplateFunction)(aSource, aArguments...);
+	}
+	template<void (*TemplateFunction)(ArgumentTypes...)>
+	static void GlobalFunction(void *, ArgumentTypes... aArguments)
+	{
+		(*TemplateFunction)(aArguments...);
+	}
+	
+	// these 2 matches appropriate template function according to what type is sent in and adds the pointer to the function and the object
+	template<class SourceType, void (SourceType:: *TemplateFunction)(ArgumentTypes...)>
+	void Connect(SourceType *aSource)
+	{
+		mySubscribers.push_back(TemplateFunctionPair(reinterpret_cast<VoidFunctionPointer>(&MemberFunction<SourceType, TemplateFunction>), aSource));
+	}
+	template<class SourceType, void (*TemplateFunction)(SourceType *, ArgumentTypes...)>
+	void Connect(SourceType *aSource)
+	{
+		mySubscribers.push_back(TemplateFunctionPair(reinterpret_cast<VoidFunctionPointer>(&StaticFunction<SourceType, TemplateFunction>), aSource));
+	}
+	// these 2 matches appropriate template function according to what type is sent in and removes a matching pointer to the function and object
+	template<class SourceType, void (SourceType:: *TemplateFunction)(ArgumentTypes...)>
+	void Disconnect(SourceType *aSource)
+	{
+		for(auto it = mySubscribers.begin(); it != mySubscribers.end(); it++)
+		{
+			if(reinterpret_cast<VoidFunctionPointer>(&MemberFunction<SourceType, TemplateFunction>) == it->first && it->second == aSource)
+			{
+				mySubscribers.erase(it);
+				break;
+			}
 		}
-		virtual void operator()(ArgumentTypes... aArguments) override {
-			std::invoke(myCallback, mySource, aArguments...);
+	}
+	template<class SourceType, void (*TemplateFunction)(SourceType *, ArgumentTypes...)>
+	void Disconnect(SourceType *aSource)
+	{
+		for(auto it = mySubscribers.begin(); it != mySubscribers.end(); it++)
+		{
+			if(reinterpret_cast<VoidFunctionPointer>(&StaticFunction<SourceType, TemplateFunction>) == it->first && it->second == aSource)
+			{
+				mySubscribers.erase(it);
+				break;
+			}
 		}
-		SourceType *mySource;
-		void(SourceType:: *myCallback)(ArgumentTypes ...);
-	};
-	template<class SourceType>
-	class StaticWrapper : public BaseWrapper {
-	public:
-		StaticWrapper(SourceType *aSource, void(*aCallback)(SourceType *, ArgumentTypes ...)) {
-			mySource = aSource;
-			myCallback = aCallback;
-		}
-		virtual void operator()(ArgumentTypes... aArguments) override {
-			std::invoke(myCallback, mySource, aArguments...);
-		}
-		SourceType *mySource;
-		void(*myCallback)(SourceType *, ArgumentTypes ...);
-	};
-	class GlobalWrapper : public BaseWrapper {
-	public:
-		GlobalWrapper(void(*aCallback)(ArgumentTypes ...)) {
-			myCallback = aCallback;
-		}
-		virtual void operator()(ArgumentTypes... aArguments) override {
-			std::invoke(myCallback, aArguments...);
-		}
-		void(*myCallback)(ArgumentTypes ...);
-	};
-	struct DataObject {
-		char myData[std::max(sizeof(MemberWrapper<Signal>), std::max(sizeof(StaticWrapper<Signal>), sizeof(GlobalWrapper)))];
-	};
-	std::vector<DataObject> mySubscribers;
+	}
 };
